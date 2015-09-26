5a4ef77d06322e9b7b6d3f5d748ef91ad0bb6ca9
maxl <randomnessSlayer@gmail.com>
2015-09-26 15:17:55 -0400

    Added Tuple Class

--- DaemonDash/src/ArticleParser.java
+++ DaemonDash/src/ArticleParser.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class ArticleParser {
-
+ArrayList<ArrayList<>>
+	
+	public ArticleParser(ArrayList<String> articles){
+		
+	}
+	
+	public ArrayList<String> getPopWordsByArticle(){
+		return null
+	}
 }
