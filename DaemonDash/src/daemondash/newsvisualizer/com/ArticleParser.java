5e5884883c8a8d5eb5976706fc47982220566ce9
maxl <randomnessSlayer@gmail.com>
2015-09-26 19:55:31 -0400

    Stuff

--- DaemonDash/src/daemondash/newsvisualizer/com/ArticleParser.java
+++ DaemonDash/src/daemondash/newsvisualizer/com/ArticleParser.java
@@ -7,50 +7,65 @@ public class ArticleParser {
 	public static final int NUM_POP_WORDS = 15;
 	private static final String[] commonWords = new String[] { "FOR", "THE", "SAID", "WHILE", "FROM", "AND", "WAS",
 			"WHERE", "WHAT", "WHEN", "WHERE", "WHY", "HOW", "THAT", "THEN", "THERE", "WHO", "ALL", "WERE", "INTO",
-			"HAD", "WILL", "WITH" };
+			"HAD", "WILL", "WITH", "ARE" };
 	private ArrayList<ArrayList<Tuple<String>>> listOfWordCountsByArticle = new ArrayList<ArrayList<Tuple<String>>>();
 	private ArrayList<Tuple<String>> listOfWordCountsTotal = new ArrayList<Tuple<String>>();
 	private ArrayList<String> mostPopWords = new ArrayList<String>();
 	private ArrayList<Tuple<String>> mostPopTuples = new ArrayList<Tuple<String>>();
+	private int progress = 0;
+	private final int numberOfTasks;
+	private boolean isDone = false;
 
-	public ArticleParser(ArrayList<String> articles) {
-		for (String s : articles) {
-			String[] strArr = s.split("\\s+");
-			ArrayList<Tuple<String>> stringCounts = new ArrayList<Tuple<String>>();
-			for (String str : strArr) {
-				boolean found = false;
-				for (Tuple<String> tuple : stringCounts) {
-					if (tuple.getKey().equals(str)) {
-						tuple.incValue();
-						found = true;
+	public ArticleParser(final ArrayList<String> articles) {
+		numberOfTasks = articles.size() * 2 + NUM_POP_WORDS;
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (String s : articles) {
+					String[] strArr = s.split("\\s+");
+					ArrayList<Tuple<String>> stringCounts = new ArrayList<Tuple<String>>();
+					for (String str : strArr) {
+						boolean found = false;
+						for (Tuple<String> tuple : stringCounts) {
+							if (tuple.getKey().equals(str)) {
+								tuple.incValue();
+								found = true;
+							}
+						}
+						if (!found && notCommonWord(str)) {
+							stringCounts.add(new Tuple<String>(str, 1));
+						}
 					}
+					listOfWordCountsByArticle.add(stringCounts);
+					progress++;
 				}
-				if (!found && notCommonWord(str)) {
-					stringCounts.add(new Tuple<String>(str, 1));
-				}
-			}
-			listOfWordCountsByArticle.add(stringCounts);
-		}
-		for (ArrayList<Tuple<String>> wordList : listOfWordCountsByArticle) {
-			for (Tuple<String> word : wordList) {
-				boolean found = false;
-				for (Tuple<String> tuple : listOfWordCountsTotal) {
-					if (tuple.getKey().equals(word.getKey())) {
-						tuple.setValue(word.getValue());
-						;
-						found = true;
+				for (ArrayList<Tuple<String>> wordList : listOfWordCountsByArticle) {
+					for (Tuple<String> word : wordList) {
+						boolean found = false;
+						for (Tuple<String> tuple : listOfWordCountsTotal) {
+							if (tuple.getKey().equals(word.getKey())) {
+								tuple.setValue(word.getValue());
+								;
+								found = true;
+							}
+						}
+						if (!found) {
+							listOfWordCountsTotal.add(new Tuple<String>(word.getKey(), word.getValue()));
+						}
 					}
+					progress++;
 				}
-				if (!found) {
-					listOfWordCountsTotal.add(new Tuple<String>(word.getKey(), word.getValue()));
+				Collections.sort(listOfWordCountsTotal);
+				for (int index = 0; index < NUM_POP_WORDS && index < listOfWordCountsTotal.size(); index++) {
+					mostPopWords.add(listOfWordCountsTotal.get(index).getKey());
+					mostPopTuples.add(listOfWordCountsTotal.get(index));
+					progress++;
 				}
+				isDone = true;
 			}
-		}
-		Collections.sort(listOfWordCountsTotal);
-		for (int index = 0; index < NUM_POP_WORDS && index < listOfWordCountsTotal.size(); index++) {
-			mostPopWords.add(listOfWordCountsTotal.get(index).getKey());
-			mostPopTuples.add(listOfWordCountsTotal.get(index));
-		}
+		});
+		t.start();
 	}
 
 	private boolean notCommonWord(String s) {
@@ -73,5 +88,13 @@ public class ArticleParser {
 
 	public ArrayList<Tuple<String>> getMostPopTuples() {
 		return mostPopTuples;
+	}
+
+	public String getProgress() {
+		return progress + " / " + numberOfTasks;
+	}
+
+	public boolean isDone() {
+		return isDone;
 	}
 }
---
+++ DaemonDash/src/daemondash/newsvisualizer/com/StaticVariables.java
@@ -0,0 +1,8 @@
+package daemondash.newsvisualizer.com;
+
+public class StaticVariables {
+	public static final int NUM_POP_WORDS = 15;
+	public static final String[] commonWords = new String[] { "FOR", "THE", "SAID", "WHILE", "FROM", "AND", "WAS",
+			"WHERE", "WHAT", "WHEN", "WHERE", "WHY", "HOW", "THAT", "THEN", "THERE", "WHO", "ALL", "WERE", "INTO",
+			"HAD", "WILL", "WITH", "ARE" };
+}
