e40da6b5687dde517f63bf2c206f63f6d7fa69bc
Weijia Cheng <weijia05@yahoo.com>
2015-09-26 15:07:33 -0400

    WebReader now reads in URLs

--- DaemonDash/src/WebReader.java
+++ DaemonDash/src/WebReader.java
@@ -1,13 +1,27 @@
+import java.io.*;
 import java.util.*;
 import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
 
 public class WebReader {
 	
-	private List<String> URLs;
 	private List<String> textOutputs;
 	
-	public WebReader(List<String> URLs) {
-		this.URLs = URLs;
+	public WebReader(List<String> URLs) throws IOException {
+		for (String URL : URLs) {
+			Document doc = Jsoup.connect(URL).get();
+			Elements paragraphs = doc.getElementsByTag("p");
+			for (Element paragraph : paragraphs) {
+				if (!(URL.contains("http://www.cnn.com") && paragraph.hasClass("zn-body__paragraph"))) {
+					paragraphs.remove(paragraph);
+				}
+			}
+		}
 	}
 
+	
+	public List<String> getOutputs() {
+		return textOutputs;
+	}
 }
