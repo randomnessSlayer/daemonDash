46ebd8b60129c12cb61073e3c38805c2eb091744
thomaspshegarty <thomas.ps.hegarty@gmail.com>
2015-09-26 15:55:14 -0400

    Working version of XMLParser that can take one search term.

--- DaemonDash/src/XMLParser.java
+++ DaemonDash/src/XMLParser.java
@@ -9,52 +9,72 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class XMLParser {
-	
+
 	private ArrayList<String> links;
-	
+
 	public XMLParser(File xmlFile){
+
+		links = new ArrayList<String>();
+		NodeList nList = createNodeList(xmlFile);
+		
+		for(int counter = 0; counter < nList.getLength(); counter++){
+
+			Node n = nList.item(counter);
+
+			if(n.getNodeType() == n.ELEMENT_NODE){
+				Element e = (Element) n;
+				String link = e.getElementsByTagName("link").item(0).getTextContent();
+				links.add(link);
+			}
+		}
+
+
+	}
+
+	public XMLParser(File xmlFile, String searchTerm){
 		
 		links = new ArrayList<String>();
+		NodeList nList = createNodeList(xmlFile);
+		searchTerm = searchTerm.toLowerCase();
 		
+		for(int counter = 0; counter < nList.getLength(); counter++){
+			
+			Node n = nList.item(counter);
+
+			if(n.getNodeType() == n.ELEMENT_NODE){
+				Element e = (Element) n;
+				String title = e.getElementsByTagName("title").item(0).getTextContent()
+						.toLowerCase();
+				String description = e.getElementsByTagName("description").item(0)
+						.getTextContent().toLowerCase();
+				if(title.contains(searchTerm) || description.contains(searchTerm)){
+					links.add(e.getElementsByTagName("link").item(0).getTextContent());
+				}
+			}
+		}
+	}
+
+	public ArrayList<String> retrieveLinks(){
+		return links;
+	}
+
+	private NodeList createNodeList(File xmlFile){
 		try{
-			
 			//create a document out of the xml file to parse the elements
-			
+
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document document = dBuilder.parse(xmlFile);
-			
+
 			document.getDocumentElement().normalize();
-			
+
 			//creates a list of nodes separated by "item" tags
-			
+
 			NodeList nList = document.getElementsByTagName("item");
-			
-			for(int counter = 0; counter < nList.getLength(); counter++){
-				
-				Node n = nList.item(counter);
-				
-				if(n.getNodeType() == n.ELEMENT_NODE){
-					Element e = (Element) n;
-					String link = e.getElementsByTagName("link").item(0).getTextContent();
-					links.add(link);
-				}
-				
-				
-			}
-			
+			return nList;
 		}catch(Exception e){
 			System.out.println(e.getMessage());
+			throw new IllegalArgumentException();
 		}
-	}
-	
-	public XMLParser(File xmlFile, String searchTerm){
-		
-	}
-	
-	public ArrayList<String> retrieveLinks(){
-
-		return links;
-
 	}
 }
