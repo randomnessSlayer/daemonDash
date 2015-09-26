66cdbf0efb640ff8e3b494c0f89e698f47f4199e
maxl <randomnessSlayer@gmail.com>
2015-09-26 16:12:56 -0400

    Initial Project Setup

---
+++ NewsVIsualizer/src/com/daemondash/newsvisualizer/background/WebReader.java
@@ -0,0 +1,14 @@
+package com.daemondash.newsvisualizer.background;
+import java.util.*;
+import org.jsoup.*;
+
+public class WebReader {
+	
+	private List<String> URLs;
+	private List<String> textOutputs;
+	
+	public WebReader(List<String> URLs) {
+		this.URLs = URLs;
+	}
+
+}
