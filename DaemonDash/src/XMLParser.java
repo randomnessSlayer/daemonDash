207876101e0a1e578f269333a9923bf3ebad3c1d
thomaspshegarty <thomas.ps.hegarty@gmail.com>
2015-09-26 15:31:56 -0400

    Updated XMLParser

--- DaemonDash/src/XMLParser.java
+++ DaemonDash/src/XMLParser.java
@@ -9,15 +9,39 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class XMLParser {
-
+	
+	ArrayList<String> links;
+	
 	public void XMLParser(File xmlFile){
+		
+		links = new ArrayList<String>();
+		
 		try{
+			
+			//create a document out of the xml file to parse the elements
+			
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document document = dBuilder.parse(xmlFile);
 			
 			document.getDocumentElement().normalize();
 			
+			//creates a list of nodes separated by "item" tags
+			
+			NodeList nList = document.getElementsByTagName("item");
+			
+			for(int counter = 0; counter < nList.getLength(); counter++){
+				
+				Node n = nList.item(counter);
+				
+				if(n.getNodeType() == n.ELEMENT_NODE){
+					Element e = (Element) n;
+					String link = e.getAttribute("link");
+					links.add(link);
+				}
+				
+				
+			}
 			
 		}catch(Exception e){
 			throw new IllegalArgumentException();
@@ -26,7 +50,7 @@ public class XMLParser {
 
 	public ArrayList<String> retrieveLinks(){
 
-		throw new IllegalArgumentException();
+		return links;
 
 	}
 }
