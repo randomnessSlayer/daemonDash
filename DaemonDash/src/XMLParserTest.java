73f71f238a875cd45183fea0c537a3ee64a7f427
thomaspshegarty <thomas.ps.hegarty@gmail.com>
2015-09-26 16:15:38 -0400

    Test inclusive of WebReader.

--- DaemonDash/src/XMLParserTest.java
+++ DaemonDash/src/XMLParserTest.java
@@ -1,17 +1,24 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class XMLParserTest {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		
 		File f = new File("C:/test.xml");
-		XMLParser xmlp = new XMLParser(f,"russia");
+		XMLParser xmlp = new XMLParser(f,"orange");
 		ArrayList<String> links = xmlp.retrieveLinks();
 		for(String s : links){
 			System.out.println(s);
 		}
 		
+		System.out.println("");
+		
+		WebReader wr = new WebReader(links);
+		for(String string : wr.getOutputs()){
+			System.out.println(string);
+		}
 	}
 	
 }
