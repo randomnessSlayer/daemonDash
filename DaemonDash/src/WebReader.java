8459b2c47724f017ada9f028eb175c3ee2688c80
Weijia Cheng <weijia05@yahoo.com>
2015-09-26 15:18:10 -0400

    Wrote WebReader constructor

--- DaemonDash/src/WebReader.java
+++ DaemonDash/src/WebReader.java
@@ -13,9 +13,17 @@ public class WebReader {
 			Document doc = Jsoup.connect(URL).get();
 			Elements paragraphs = doc.getElementsByTag("p");
 			for (Element paragraph : paragraphs) {
+				String output = "";
+				
 				if (!(URL.contains("http://www.cnn.com") && paragraph.hasClass("zn-body__paragraph"))) {
 					paragraphs.remove(paragraph);
+				} else {
+					String text = paragraph.text();
+					text.replaceAll("[\\p{Punct}&&[^\\0x27]]", " ");
+					output += text;
 				}
+				
+				textOutputs.add(output);
 			}
 		}
 	}
